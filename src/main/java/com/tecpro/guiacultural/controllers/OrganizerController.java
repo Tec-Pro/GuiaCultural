fdf8e6337130dcc4fc04f1e060f6e0077da1123c
jheredia <joaquinheredia93@gmail.com>
2015-10-03 00:52:44 -0300

    Test3

--- src/main/java/com/tecpro/guiacultural/controllers/OrganizerController.java
+++ src/main/java/com/tecpro/guiacultural/controllers/OrganizerController.java
@@ -6,7 +6,17 @@
 package com.tecpro.guiacultural.controllers;
 
 import com.tecpro.guiacultural.models.Organizer;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,13 +27,41 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class OrganizerController {
 
-    @RequestMapping(value="/organizer")
-    public Organizer getOrganizer(@RequestParam(value="id", defaultValue="1") int id){
-        Organizer org = new Organizer();
-        return org.get(id);
+    @RequestMapping(value="/organizer", method = RequestMethod.GET)
+    public Organizer getOrganizer(@RequestParam(value = "id", defaultValue = "1") int id) {
+        try {
+            Connection connection = getConnection();
+            Statement stmt = connection.createStatement();
+            try {
+                ResultSet rs = stmt.executeQuery("SELECT * FROM organizer WHERE id=" + id);
+                return new Organizer(rs.getInt(1),rs.getString(2));
+            } catch (SQLException ex) {
+                Logger.getLogger(OrganizerController.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(OrganizerController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
     }
-    
-    
 
-   
+
+    
+    private Connection getConnection() {
+        try {
+            URI dbUri = new URI(System.getenv("DATABASE_URL"));
+
+            String username = dbUri.getUserInfo().split(":")[0];
+            String password = dbUri.getUserInfo().split(":")[1];
+            String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + dbUri.getPath();
+
+            return DriverManager.getConnection(dbUrl, username, password);
+        } catch (URISyntaxException uri) {
+            System.out.println("URI Exception: " + uri.getReason() + " Message: " + uri.getMessage());
+            System.out.println("On input: " + uri.getInput());
+        } catch (SQLException sql) {
+            System.out.println("SQL Exception: " + sql.getSQLState());
+        }
+        return null;
+    }
 }
