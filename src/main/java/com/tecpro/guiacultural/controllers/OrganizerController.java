c8c54b3240a2282a0c8964483e139484061c6ef7
jheredia <joaquinheredia93@gmail.com>
2015-10-05 02:44:58 -0300

    Test with activejdbc

--- src/main/java/com/tecpro/guiacultural/controllers/OrganizerController.java
+++ src/main/java/com/tecpro/guiacultural/controllers/OrganizerController.java
@@ -34,21 +34,20 @@ public class OrganizerController {
     @RequestMapping(value = "/organizer", method = RequestMethod.GET)
     public String getOrganizer(@RequestParam(value = "id", defaultValue = "1") int id) {
         CRUDOrganizer crud = new CRUDOrganizer();
-        Organizer organizer = crud.get(id);
-        return organizer.toJson(true);
+        return crud.get(id).toJson(true);
     }
 
     @RequestMapping(value = "/organizers", method = RequestMethod.GET)
-    public LazyList<Model> getOrganizers() {
+    public String getOrganizers() {
         CRUDOrganizer crud = new CRUDOrganizer();
-        return crud.list();
+        return crud.list().toJson(true);
     }
 
     @RequestMapping(value = "/organizer", method = RequestMethod.PUT)
-    public Organizer createOrganizer(@RequestParam(value = "name") String name) {
+    public String createOrganizer(@RequestParam(value = "name") String name) {
         if (!name.isEmpty()) {
             CRUDOrganizer crud = new CRUDOrganizer();
-            return crud.create(name);
+            return crud.create(name).toJson(true);
         }
         return null;
     }
