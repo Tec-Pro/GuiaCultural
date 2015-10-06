aaf70ea2de20cebd60615e7fa79a91f9c4456bbf
jheredia <joaquinheredia93@gmail.com>
2015-10-06 17:15:24 -0300

    Beta controllers

--- src/main/java/com/tecpro/guiacultural/controllers/LiteraryCycleController.java
+++ src/main/java/com/tecpro/guiacultural/controllers/LiteraryCycleController.java
@@ -11,11 +11,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  *
  * @author joako
  */
+@RestController
 public class LiteraryCycleController {
     @RequestMapping(value = "/cycles", method = RequestMethod.GET)
     public String index() {
--- src/main/java/com/tecpro/guiacultural/controllers/VisualArtsController.java
+++ src/main/java/com/tecpro/guiacultural/controllers/VisualArtsController.java
@@ -11,11 +11,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  *
  * @author joako
  */
+@RestController
+
 public class VisualArtsController {
     
     @RequestMapping(value = "/arts", method = RequestMethod.GET)
